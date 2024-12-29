32287997766a0b195f69357db7f88767201e6716
Harshith20000 <harshithappu85@gmail.com>
2024-12-29 14:52:46 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,1 +1,12 @@
+import requests
+
+# Replace YOUR_API_KEY with the actual API key you obtained
+api_key = "ccc04b20b61a36cfa0362b1b2e656209"
+city = "Bangalore"
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+print(data)
 
